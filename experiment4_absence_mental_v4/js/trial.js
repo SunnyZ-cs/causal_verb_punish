1684c724a01a513b113a107564a725be41700fc4
Louise Li <129934489+CSLouise@users.noreply.github.com>
2026-02-17 22:24:01 -0800

    exp4 mental v4

---
+++ experiment4_absence_mental_v4/js/trial.js
@@ -0,0 +1,54 @@
+const trials = {
+    "instruction": "images/instructions.png",
+    "scenarios": {
+        "hurt": {
+            "story": [
+                "images/hurt_mental/Slide54.png",
+                "images/hurt_mental/Slide55.png",
+                "images/hurt_mental/Slide56.png",
+                "images/hurt_mental/Slide57.png",
+                "images/hurt_mental/Slide58.png",
+                "images/hurt_mental/Slide59.png",
+                "images/hurt_mental/Slide60.png",
+                "images/hurt_mental/Slide61.png",
+                "images/hurt_mental/Slide62.png",
+                "images/hurt_mental/Slide63.png",
+                "images/hurt_mental/Slide64.png"
+            ],
+            "questions": {
+                "cause": "images/hurt_mental/q_cause_hurt.png",
+                "lexical": "images/hurt_mental/q_hurt.png"
+            },
+            "response_options": {
+                "distal": "images/hurt_mental/response_distal.png",
+                "proximal": "images/hurt_mental/response_proximal.png"
+            }
+        },
+        "shock": {
+            "story": [
+                "images/shock_mental/Slide69.png",
+                "images/shock_mental/Slide70.png",
+                "images/shock_mental/Slide71.png",
+                "images/shock_mental/Slide72.png",
+                "images/shock_mental/Slide73.png",
+                "images/shock_mental/Slide74.png",
+                "images/shock_mental/Slide75.png",
+                "images/shock_mental/Slide76.png",
+                "images/shock_mental/Slide77.png",
+                "images/shock_mental/Slide78.png",
+                "images/shock_mental/Slide79.png",
+                "images/shock_mental/Slide80.png",
+                "images/shock_mental/Slide81.png",
+                "images/shock_mental/Slide82.png"
+            ],
+            "questions": {
+                "cause": "images/shock_mental/q_cause_shock.png",
+                "lexical": "images/shock_mental/q_shock.png"
+            },
+            "response_options": {
+                "distal": "images/shock_mental/response_distal.png",
+                "proximal": "images/shock_mental/response_proximal.png"
+            }
+        }
+    }
+}
