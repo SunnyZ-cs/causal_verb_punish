b94a36fdd19b1bc49fdb80527e40ad5f63325e8c
Louise Li <129934489+CSLouise@users.noreply.github.com>
2026-04-08 14:19:18 -0700

    update punishment study

---
+++ experiment2_accidental_v4/js/trial.js
@@ -0,0 +1,42 @@
+const trials = {
+    "instructions": "images/instructions.png",
+    "scenarios": {
+        "bike": {
+            "story": [
+                "images/bike/bike_adults_accident.001.png",
+                "images/bike/bike_adults_accident.002.png",
+                "images/bike/bike_adults_accident.003.png",
+                "images/bike/bike_adults_accident.004.png",
+                "images/bike/bike_adults_accident.005.png",
+                "images/bike/bike_adults_accident.006.png",
+                "images/bike/bike_adults_accident.007.png",
+                "images/bike/bike_adults_accident.008.png"
+            ],
+            "questions": {
+                "cause": "images/bike/bike_adults_accident_q_cause.png",
+                "simple": "images/bike/bike_adults_accident_q_simple.png",
+                "fault": "images/bike/bike_adults_accident_q_fault.png",
+                "punish": "images/bike/bike_adults_accident_q_punish.png"
+            }
+        },
+        "mirror": {
+            "story": [
+                "images/mirror/mirror_adults_accident.001.png",
+                "images/mirror/mirror_adults_accident.002.png",
+                "images/mirror/mirror_adults_accident.003.png",
+                "images/mirror/mirror_adults_accident.004.png",
+                "images/mirror/mirror_adults_accident.005.png",
+                "images/mirror/mirror_adults_accident.006.png",
+                "images/mirror/mirror_adults_accident.007.png",
+                "images/mirror/mirror_adults_accident.008.png",
+                "images/mirror/mirror_adults_accident.009.png"
+            ],
+            "questions": {
+                "cause": "images/mirror/mirror_adults_accident_q_cause.png",
+                "simple": "images/mirror/mirror_adults_accident_q_simple.png",
+                "fault": "images/mirror/mirror_adults_accident_q_fault.png",
+                "punish": "images/mirror/mirror_adults_accident_q_punish.png"
+            }
+        }
+    }
+}
---
+++ experiment2_intentional_v4/js/trial.js
@@ -0,0 +1,46 @@
+const trials = {
+    "instructions": "images/instructions.png",
+    "scenarios": {
+        "bike": {
+            "story": [
+                "images/bike/punish.001.png",
+                "images/bike/punish.002.png",
+                "images/bike/punish.003.png",
+                "images/bike/punish.004.png",
+                "images/bike/punish.005.png",
+                "images/bike/punish.006.png",
+                "images/bike/punish.007.png",
+                "images/bike/punish.008.png",
+                "images/bike/punish.009.png",
+                "images/bike/punish.010.png",
+                "images/bike/punish.011.png"
+            ],
+            "questions": {
+                "cause": "images/bike/q_cause.png",
+                "simple": "images/bike/q_simple.png",
+                "fault": "images/bike/q_fault.png",
+                "punish": "images/bike/q_punish.png"
+            }
+        },
+        "mirror": {
+            "story": [
+                "images/mirror/punish.016.png",
+                "images/mirror/punish.017.png",
+                "images/mirror/punish.018.png",
+                "images/mirror/punish.019.png",
+                "images/mirror/punish.020.png",
+                "images/mirror/punish.021.png",
+                "images/mirror/punish.022.png",
+                "images/mirror/punish.023.png",
+                "images/mirror/punish.024.png",
+                "images/mirror/punish.025.png"
+            ],
+            "questions": {
+                "cause": "images/mirror/q_cause.png",
+                "simple": "images/mirror/q_simple.png",
+                "fault": "images/mirror/q_fault.png",
+                "punish": "images/mirror/q_punish.png"
+            }
+        }
+    }
+}
